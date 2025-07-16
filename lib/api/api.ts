5cd14eaa6b021f03c806de7143efc0b1d1c169c3
Vitalii Kryshtal <krishtal.vtalk@ukr.net>
2025-07-16 19:03:19 +0300

    hw

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { Note } from "../types/note";
+import type { Note } from "../../types/note";
 
 export const nextServer = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
